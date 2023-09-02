64867033347aa0dd116eac689e994f17b522ac37
rosedoria123 <113267533+rosedoria123@users.noreply.github.com>
2023-09-02 10:30:19 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '866223261515962',
+        clientSecret: '397a80dc9a6f81ce9ad0b77f7b6347fe'
+    }
+}
